35fe98b8238fc1b75111ace2b494163ac4df2ee8
Dugald Wilson <dug.wilson@insight.com>
2020-12-05 10:23:30 -0500

    Day 5

---
+++ Day5/Day5/BinarySeat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day5
+{
+    public class BinarySeat
+    {
+        public BinarySeat(string data)
+        {
+            string row = data.Substring(0, 7).Replace("F","0").Replace("B","1");
+            Row = Convert.ToInt32(row, 2);
+            string seat = data.Substring(7).Replace("R","1").Replace("L","0");
+            Seat = Convert.ToInt32(seat, 2);
+        }
+
+        public int Row { get; set; }
+        public int Seat { get; set; }
+
+        public int SeatId()
+        {
+            return Row * 8 + Seat;
+        }
+    }
+}
---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<BinarySeat> seats = new List<BinarySeat>();
+            var stream = File.Open("Input.txt", FileMode.Open);
+            var reader = new StreamReader(stream);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                seats.Add(new BinarySeat(line));
+            }
+
+            Console.WriteLine($"Highest score: {seats.Max(x => x.SeatId())}");
+
+            var sortedSeats = seats.OrderBy(x => x.SeatId()).ToArray();
+            for (int i = 0; i < sortedSeats.Length -1; i++)
+            {
+                if (sortedSeats[i].SeatId() + 2 == sortedSeats[i+1].SeatId())
+                    Console.WriteLine($"Possible Seat between {sortedSeats[i].SeatId()} and {sortedSeats[i + 1].SeatId()}");
+            }
+            Console.Read();
+        }
+    }
+}
