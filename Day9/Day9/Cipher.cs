aeac748247826cca94510999ef844410914473b4
Dugald Wilson <dug.wilson@insight.com>
2020-12-09 08:47:48 -0500

    Day 9

---
+++ Day9/Day9/Cipher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day9
+{
+    public class Cipher
+    {
+        public long[] Values { get; set; }
+
+        public Cipher(string[] input)
+        {
+            Values = input.Select(x => Convert.ToInt64(x)).ToArray();
+        }
+
+        public long Weakness()
+        {
+            for (int i = 25; i < Values.Length; i++)
+            {
+                if (!HasSum(Values[i], Values[(i - 25)..i]))
+                    return Values[i];
+            }
+
+            return -1;
+        }
+
+        public long RangeWeakness(long target)
+        {
+            for (int i = 0; i < Values.Length; i ++)
+            {
+                for (int j = i + 1; j < Values.Length; j++)
+                {
+                    if (Values[i..j].Sum() == target)
+                        return Values[i..j].Min() + Values[i..j].Max();
+                    if (Values[i..j].Sum() > target)
+                        break;
+                }
+            }
+            return 1;
+        }
+
+        private bool HasSum(long target, long[] possibleValues)
+        {
+            for (int i = 0; i < possibleValues.Length; i++)
+            {
+                for (int j = i + 1; j < possibleValues.Length; j++)
+                {
+                    if (possibleValues[i] + possibleValues[j] == target)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            var cipher = new Cipher(lines);
+            Console.WriteLine($"The cipher's weakness is {cipher.Weakness()}");
+            Console.WriteLine($"The cipher's range weakness is {cipher.RangeWeakness(cipher.Weakness())}");
+        }
+    }
+}
